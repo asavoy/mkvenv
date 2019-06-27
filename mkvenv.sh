fc68eadac7a91312b4eb6a0676774dd52768ae31
Alvin Savoy <asavoy@users.noreply.github.com>
2019-06-28 01:27:05 +1000

    shellcheck

--- mkvenv.sh
+++ mkvenv.sh
@@ -11,7 +11,7 @@ function mkvenv() {
   hash virtualenv 2>/dev/null || { echo "virtualenv not found"; return 1; }
   hash direnv 2>/dev/null || { echo "direnv not found"; return 1; }
 
-  if [[ -f .python-version && $(cat .python-version) != $version ]]; then
+  if [[ -f .python-version && $(cat .python-version) != "$version" ]]; then
     echo "$version does not match .python-version"
     return 1
   fi
@@ -21,7 +21,7 @@ function mkvenv() {
   fi
 
   pyenv local "$version" \
-    && virtualenv --python $(pyenv which python) "$venvdir" \
+    && virtualenv --python "$(pyenv which python)" "$venvdir" \
     && echo "source \"$venvdir/bin/activate\"" >> .envrc \
     && direnv allow
 }
